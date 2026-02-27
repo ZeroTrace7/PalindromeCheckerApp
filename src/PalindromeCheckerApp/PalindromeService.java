a2f4cbcbb46289982440b95e69f65817dde70310
ZeroTrace7 <shreyashgupta999@gmail.com>
2026-02-27 09:19:24 +0530

    UC12: Strategy pattern palindrome checker

---
+++ src/PalindromeCheckerApp/PalindromeService.java
@@ -0,0 +1,11 @@
+public class PalindromeService {
+    private final PalindromeStrategy strategy;
+
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean checkPalindrome(String text) {
+        return strategy.isPalindrome(text);
+    }
+}
